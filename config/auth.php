0b3aa0e224412486a5e01311499022210361730a
clarenceevanw <clarenceevan0907@gmail.com>
2026-01-27 20:56:47 +0700

    fix: configure auth

--- config/auth.php
+++ config/auth.php
@@ -40,10 +40,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        'admin' => [
-            'driver' => 'session',
-            'provider' => 'admins',
-        ]
+        'api' => ['driver' => 'sanctum', 'provider' => 'users'],
     ],
 
     /*
