e2a6d95a65af34754d32529fdefeddb9803075bc
clarenceevanw <clarenceevan0907@gmail.com>
2026-01-31 16:07:36 +0700

    feat: login admin route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::post('/login/admin', [AdminController::class, 'login']);
