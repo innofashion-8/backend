65f45faf278ccc19e5a3634bbc2375dec7da7100
clarenceevanw <clarenceevan0907@gmail.com>
2026-02-10 09:11:17 +0700

    feat: division seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            DivisionSeeder::class,
             EventSeeder::class,
             CompetitionSeeder::class,
             UserSeeder::class
---
+++ database/seeders/DivisionSeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Division;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DivisionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $divisions = [
+            [
+                'name' => 'Badan Pengurus Harian',
+                'slug' => 'bph',
+            ],
+            [
+                'name' => 'Kepala Bidang',
+                'slug' => 'kabid',
+            ],
+            [
+                'name' => 'Transportasi dan Keamanan',
+                'slug' => 'transkapman',
+            ],
+            [
+                'name' => 'Sekretariat',
+                'slug' => 'sekret',
+            ],
+            [
+                'name' => 'Konsumsi Kesehatan',
+                'slug' => 'konkes',
+            ],
+            [
+                'name' => 'Lomba',
+                'slug' => 'lomba',
+            ],
+            [
+                'name' => 'Pameran',
+                'slug' => 'pameran',
+            ],
+            [
+                'name' => 'Acara',
+                'slug' => 'acara',
+            ],
+            [
+                'name' => 'Information Technology',
+                'slug' => 'it',
+            ],
+            [
+                'name' => 'Sponsorship',
+                'slug' => 'sponsor',
+            ],
+            [
+                'name' => 'Public Relation',
+                'slug' => 'pr',
+            ],
+            [
+                'name' => 'Creative',
+                'slug' => 'creative',
+            ],
+        ];
+
+        foreach ($divisions as $division) {
+            Division::create($division);
+        }
+    }
+}
