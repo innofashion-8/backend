c32ada8d54a02f3ab7327816b2fa98bf527258af
clarenceevanw <clarenceevan0907@gmail.com>
2026-02-10 09:17:59 +0700

    feat: admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use App\Models\Division;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $divisions = Division::all()->keyBy('slug');
+        $adminsData = [
+            [
+                'name' => 'Clarence Evan Wijaya',
+                'nrp' => 'C14240069',
+                'email' => 'c14240069@john.petra.ac.id',
+                'division_slug' => 'it',
+            ],
+        ];
+
+        foreach ($adminsData as $adminData) {
+            if (isset($divisions[$adminData['division_slug']])) {
+                Admin::create([
+                    'name' => $adminData['name'],
+                    'nrp' => $adminData['nrp'],
+                    'email' => $adminData['email'],
+                    'division_id' => $divisions[$adminData['division_slug']]->id,
+                ]);
+            }
+        }
+
+    }
+}
