fc1a91c1cb4d3444c5bbea4d483e988800a3d348
clarenceevanw <clarenceevan0907@gmail.com>
2026-02-22 17:35:00 +0700

    fix: add verified by in update status

--- app/Data/UpdateStatusDTO.php
+++ app/Data/UpdateStatusDTO.php
@@ -6,6 +6,7 @@ class UpdateStatusDTO
 {
     public function __construct(
         // ID dari EventRegistration atau CompetitionRegistration
+        public readonly ?string $verifiedBy,
         public readonly string $registrationId,
         public readonly string $status,
         public readonly ?string $rejection_reason,
