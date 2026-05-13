e3d9d280d7333ba5cae0fae0a4cc6d80a655f652
clarenceevanw <clarenceevan0907@gmail.com>
2026-05-13 15:26:26 +0700

    feat: resource for getDepartment

---
+++ app/Http/Resources/DivisionResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DivisionResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'admins_count' => $this->whenCounted('admins'),
+            'created_at' => $this->created_at?->toISOString(),
+            'updated_at' => $this->updated_at?->toISOString(),
+        ];
+    }
+}
