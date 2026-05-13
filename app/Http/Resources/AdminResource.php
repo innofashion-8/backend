1ca803cedd8d280cb6d184e40bba5097fb5f0cb7
clarenceevanw <clarenceevan0907@gmail.com>
2026-05-13 15:26:15 +0700

    feat: resource for get admin

---
+++ app/Http/Resources/AdminResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AdminResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'nrp' => $this->nrp,
+            'email' => $this->email,
+            'division_id' => $this->division_id,
+            'division' => new DivisionResource($this->whenLoaded('division')),
+            'roles' => $this->whenLoaded('roles', function () {
+                return $this->roles->map(function ($role) {
+                    return [
+                        'id' => $role->id,
+                        'name' => $role->name,
+                    ];
+                });
+            }),
+            'permissions' => $this->whenLoaded('permissions', function () {
+                return $this->permissions->pluck('name');
+            }),
+            'created_at' => $this->created_at?->toISOString(),
+            'updated_at' => $this->updated_at?->toISOString(),
+        ];
+    }
+}
