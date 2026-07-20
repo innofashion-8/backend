9d6dc9befa562beb4db8720aa769405bb847116f
clarenceevanw <clarenceevan0907@gmail.com>
2026-07-20 19:07:12 +0700

    feat: only for verified restyling registration

--- app/Services/Eligibility/RestylingEligibilityChecker.php
+++ app/Services/Eligibility/RestylingEligibilityChecker.php
@@ -3,6 +3,7 @@
 namespace App\Services\Eligibility;
 
 use App\Contracts\EventEligibilityCheckerInterface;
+use App\Enum\StatusRegistration;
 use App\Models\Event;
 use App\Models\User;
 use App\Models\CompetitionRegistration;
@@ -13,7 +14,7 @@ class RestylingEligibilityChecker implements EventEligibilityCheckerInterface
     public function isEligible(Event $event, User $user): bool
     {
         $isLeader = CompetitionRegistration::where('user_id', $user->id)
-            ->whereIn('status', ['VERIFIED', 'PENDING'])
+            ->where('status', StatusRegistration::VERIFIED)
             ->whereHas('competition', function($q) {
                 $q->where('name', 'LIKE', '%RESTYLING%')
                   ->orWhere('name', 'LIKE', '%STYLING%');
@@ -26,7 +27,7 @@ class RestylingEligibilityChecker implements EventEligibilityCheckerInterface
         // 2. Cek apakah user adalah Anggota (user_id di competition_members)
         $isMember = CompetitionMember::where('user_id', $user->id)
             ->whereHas('registration', function($q) {
-                $q->whereIn('status', ['VERIFIED', 'PENDING'])
+                $q->where('status', StatusRegistration::VERIFIED)
                   ->whereHas('competition', function($q2) {
                       $q2->where('name', 'LIKE', '%RESTYLING%')
                          ->orWhere('name', 'LIKE', '%STYLING%');
